671e0e0811f658aedd0a234dc4659280ade99be2
hiddro <edd.ckalb@gmail.com>
2024-01-30 01:56:51 -0500

    frontend angular + angular material

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CustomValidators } from 'src/assets/utils/custom-validator';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'frontend-automation-bcr';
+
+  firstFormGroup = this._formBuilder.group({
+    entidad: ['', Validators.required],
+    rol: ['', Validators.required],
+    inicioEjecucion: ['', Validators.required],
+    finEjecucion: ['', Validators.required],
+    duraProgramado: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+    duraCorrectivo: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+    duraEmergencias: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+    cantProgramado: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+    cantCorrectivo: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+    cantEmergencias: ['', [Validators.required, Validators.pattern(/^\d+$/)]]
+  });
+  secondFormGroup = this._formBuilder.group({
+    codMantenimiento: ['', Validators.required],
+    inicioMantenimiento: ['', Validators.required],
+    finMantenimiento: ['', Validators.required],
+    horaInicio: ['', Validators.required],
+    horaFin: ['', Validators.required],
+    duracion: ['', Validators.required],
+    tipoMantenimiento: ['', Validators.required],
+    motivo: ['', Validators.required],
+    probDetectados: ['', Validators.required],
+    nodisDegradacion: ['', Validators.required],
+    paseProduccion: ['', Validators.required],
+    otrEntidades: ['', Validators.required],
+    comentarios: ['', Validators.required]
+  });
+  isEditable = true;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+
+  }
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSnackBarModule,
+    ReactiveFormsModule,
+    FormsModule,
+    // Angular Material Imports
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatStepperModule,
+    CommonModule
+  ],
+  providers: [
+    // {
+    //   provide: STEPPER_GLOBAL_OPTIONS,
+    //   useValue: { showError: true },
+    // }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
